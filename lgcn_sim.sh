5bed108411b392fec8ba781a29e6d7f0c495dff6
cddavis93 <cd_davis93@yahoo.com>
2025-09-11 15:52:13 -0500

    updating lgcn egnn sim scripts and adding A100 bypass config file and definition

--- lgcn_sim.sh
+++ lgcn_sim.sh
@@ -4,6 +4,8 @@ source ./gpu-simulator/setup_environment.sh
 ./util/job_launching/run_simulations.py -B light-gcn -C A100-SASS -T ./hw_run/traces/device-2/12.8/ -N lgcn_A100-SASS
 ./util/job_launching/run_simulations.py -B light-gcn -C A100_bypass -T ./hw_run/traces/device-2/12.8/ -N lgcn_A100-bypass
 
-#./util/job_launching/run_simulations.py -B light-gcn -C A100-PTX -N lgcn_A100_PTX
+#./util/job_launching/monitor_func_test.py -v -N lgcn_A100-SASS
+./util/job_launching/monitor_func_test.py -v -N lgcn_A100-bypass
 
 #./util/job_launching/get_stats.py -k -K -s util/job_launching/stats/stats_all.yml -N lgcn_A100-SASS | tee stats_lgcn_A100_SASS-per-kernel-instance.csv
+./util/job_launching/get_stats.py -k -K -s util/job_launching/stats/stats_all.yml -N lgcn_A100-bypass | tee stats_lgcn_A100-bypass-per-kernel-instance.csv
